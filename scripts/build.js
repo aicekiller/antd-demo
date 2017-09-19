bf4d22c93f5f48f39977b80f31a4711bca1c0fb6
aicekiller <1017634520@qq.com>
2017-09-19 16:40:54 +0800

    add

---
+++ scripts/build.js
@@ -0,0 +1,173 @@
+'use strict';
+
+// Do this as the first thing so that any code reading it knows the right env.
+/**
+ * process.env包含当前shell的所有环境变量，process.env.HOME(返回用户主目录)
+ * 新建两个变量(BABEL_ENV,NODE_ENV)，确定当前变量为生产环境
+ * @type {string}
+ */
+process.env.BABEL_ENV = 'production';
+process.env.NODE_ENV = 'production';
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+/**
+ * process.on() 监听unhandledRejection事件
+ */
+process.on('unhandledRejection', err => {
+    throw err;
+});
+
+// Ensure environment variables are read.
+require('../config/env');
+
+const path = require('path');
+const chalk = require('chalk');//终端颜色设置
+const fs = require('fs-extra');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.prod');
+const paths = require('../config/paths');
+/**
+ * @type {checkRequiredFiles(files: Array<string>): boolean}
+ * @description 确保传递的文件都存在
+ */
+const checkRequiredFiles = require('react-dev-utils/checkRequiredFiles');
+/**
+ * 从webpack stats对象提取和预处理警告和错误消息
+ * @type {formatWebpackMessages}
+ */
+const formatWebpackMessages = require('react-dev-utils/formatWebpackMessages');
+/**
+ * 在项目建成后打印托管说明
+ * @type {printHostingInstructions}
+ */
+const printHostingInstructions = require('react-dev-utils/printHostingInstructions');
+/**
+ * 在传递的buildFolder中捕获JS和CSS资源大小。保存结果值，以便在构建后进行比较
+ */
+const {measureFileSizesBeforeBuild, printFileSizesAfterBuild} = require('react-dev-utils/FileSizeReporter');
+/**
+ * 预处理一些已知的构建错误
+ */
+const printBuildError = require('react-dev-utils/printBuildError');
+//判断yarn.lock文件是否存在
+const useYarn = fs.existsSync(paths.yarnLockFile);
+
+// These sizes are pretty large. We'll warn for bundles exceeding them.
+// 打包文件的最大值
+const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
+const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
+
+// Warn and crash if required files are missing
+//如果src/index.js和public/index.html不存在，build失败，退出当前进程
+if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
+    process.exit(1);
+}
+
+// First, read the current file sizes in build directory.
+// This lets us display how much they changed later.
+measureFileSizesBeforeBuild(paths.appBuild)
+    .then(previousFileSizes => {
+        // Remove all content but keep the directory so that
+        // if you're in it, you don't end up in Trash
+        fs.emptyDirSync(paths.appBuild);
+        // Merge with the public folder
+        copyPublicFolder();
+        // Start the webpack build
+        return build(previousFileSizes);
+    })
+    .then(
+        ({stats, previousFileSizes, warnings}) => {
+            if (warnings.length) {
+                console.log(chalk.yellow('Compiled with warnings.\n'));
+                console.log(warnings.join('\n\n'));
+                console.log(
+                    '\nSearch for the ' +
+                    chalk.underline(chalk.yellow('keywords')) +
+                    ' to learn more about each warning.'
+                );
+                console.log(
+                    'To ignore, add ' +
+                    chalk.cyan('// eslint-disable-next-line') +
+                    ' to the line before.\n'
+                );
+            } else {
+                console.log(chalk.green('Compiled successfully.\n'));
+            }
+
+            console.log('File sizes after gzip:\n');
+            printFileSizesAfterBuild(
+                stats,
+                previousFileSizes,
+                paths.appBuild,
+                WARN_AFTER_BUNDLE_GZIP_SIZE,
+                WARN_AFTER_CHUNK_GZIP_SIZE
+            );
+
+            const appPackage = require(paths.appPackageJson);
+            const publicUrl = paths.publicUrl;
+            const publicPath = config.output.publicPath;
+            const buildFolder = path.relative(process.cwd(), paths.appBuild);
+            printHostingInstructions(
+                appPackage,
+                publicUrl,
+                publicPath,
+                buildFolder,
+                useYarn
+            );
+        },
+        err => {
+            console.log(chalk.red('Failed to compile.\n'));
+            printBuildError(err);
+            process.exit(1);
+        }
+    );
+
+// Create the production build and print the deployment instructions.
+function build(previousFileSizes) {
+    console.log('Creating an optimized production build...');
+
+    let compiler = webpack(config);
+    return new Promise((resolve, reject) => {
+        compiler.run((err, stats) => {
+            if (err) {
+                return reject(err);
+            }
+            const messages = formatWebpackMessages(stats.toJson({}, true));
+            if (messages.errors.length) {
+                // Only keep the first error. Others are often indicative
+                // of the same problem, but confuse the reader with noise.
+                if (messages.errors.length > 1) {
+                    messages.errors.length = 1;
+                }
+                return reject(new Error(messages.errors.join('\n\n')));
+            }
+            if (
+                process.env.CI &&
+                (typeof process.env.CI !== 'string' ||
+                process.env.CI.toLowerCase() !== 'false') &&
+                messages.warnings.length
+            ) {
+                console.log(
+                    chalk.yellow(
+                        '\nTreating warnings as errors because process.env.CI = true.\n' +
+                        'Most CI servers set it automatically.\n'
+                    )
+                );
+                return reject(new Error(messages.warnings.join('\n\n')));
+            }
+            return resolve({
+                stats,
+                previousFileSizes,
+                warnings: messages.warnings,
+            });
+        });
+    });
+}
+
+function copyPublicFolder() {
+    fs.copySync(paths.appPublic, paths.appBuild, {
+        dereference: true,
+        filter: file => file !== paths.appHtml,
+    });
+}

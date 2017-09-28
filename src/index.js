076e7afbf41dba306e2945acee9ff9b8582e38be
aicekiller <1017634520@qq.com>
2017-09-28 17:52:59 +0800

    modfiy

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 
 import routesconfig from './routers';
 
+const history = createHistory();
+
 ReactDOM.render(
-    <Router>
+    <Router history={history}>
         <div>
             {
-                routesconfig.map((item, index) => {
+                routesconfig.map((item, index, arr) => {
                     return (
                         <Route key={index} path={item.path} component={item.component}></Route>
                     )
--- src/routers.js
+++ src/routers.js
@@ -1,13 +1,19 @@
 import CommentApp from './page/comments/commentApp';
 import Game from './page/game/game';
+import Index from './page/theme/Index';
 
 const routesconfig = [
     {
-        path:"/comments",
-        component:CommentApp
+        path: "/comments",
+        component: CommentApp
     },
-    {   path:"/game",
-        component:Game
+    {
+        path: "/game",
+        component: Game
+    },
+    {
+        path: "/theme",
+        component: Index
     }
 ];
 
